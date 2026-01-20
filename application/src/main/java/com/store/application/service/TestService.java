cad79b3ad0fa016009f14e44c85b3d5257a17096
ArtemMakovskyy <photomak79@gmail.com>
2026-01-20 15:05:14 +0200

    ---

--- application/src/main/java/com/store/application/service/TestService.java
+++ application/src/main/java/com/store/application/service/TestService.java
@@ -1,6 +1,7 @@
 package com.store.application.service;
 
 import lombok.SneakyThrows;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,13 @@ public class TestService {
 
     @SneakyThrows
     public String getResultWIthPause(int sec, String message){
+        Thread.sleep(sec * 1000L);
+        return "success " + message + " wait for " + sec + " seconds";
+    }
+
+    @SneakyThrows
+    @Async
+    public String getResultWIthPauseAsync(int sec, String message){
         Thread.sleep(sec * 1000L);
         return "success " + message + " wait for " + sec + " seconds";
     }
